360c3bbb2f704dfed64651f41e8b37bfb2982f77
Felix E. Orduz G. <felixernestoorduzgrimaldo@Felixs-MacBook-Pro.local>
2023-11-12 16:39:26 -0500

    refactor(gcpfiles.py): remove unused get method in GCPFiles class feat(gcpfiles.py): update upload_file method in GCPFiles class to include a prefix for the destination blob name

--- api/resources/gcpfiles.py
+++ api/resources/gcpfiles.py
@@ -6,25 +6,12 @@ class GCPFiles(Resource):
     def __init__(self):
         self.client = storage.Client()
 
-        self.bucket_key = os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/felixernestoorduzgrimaldo/Documents/Estudio/MISO/repos/MISO-Conversor-Formato/api/file.json"
+        self.bucket_key = os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "file.json"
         self.bucket_name = os.environ.get('GCP_BUCKET_NAME', 'miso-4204-feog-exp1')
-
-    def get(self, filetype, filename):
-        if filetype not in ["original", "processed"]:
-            return {"message": "Tipo de archivo no válido."}, 400
-
-        bucket = self.client.bucket(self.bucket_name)
-        blob = bucket.blob(filename)
-
-        # Generar URL de descarga
-        url = blob.generate_signed_url(version="v4",
-                                       expiration=datetime.timedelta(minutes=15),
-                                       method="GET")
-
-        return {"url": url}
 
     def upload_file(self, file, destination_blob_name):
         """Sube un archivo al bucket de GCP."""
+        destination_blob_name = f"uploaded/{destination_blob_name}"
         bucket = self.client.bucket(self.bucket_name)
         blob = bucket.blob(destination_blob_name)
 
